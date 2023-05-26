d62e843cd163606685d36f6c487fde09ad275879
GuillaumeS <gsoulie@groupe-isia.com>
2023-05-26 16:50:03 +0200

    signal integration example

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, Input, inject } from '@angular/core';
+import { Component, DestroyRef, Input, inject, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,17 +8,35 @@ import { CommonModule } from '@angular/common';
   template: `
   <h1>Angular Minimal template</h1>
   <p>--> {{ routeInfo }}</p>
-  <button class="btn btn-plain">Test button</button>
+  <h2>Signal integration</h2>
+  <ul>
+    <li><span>Counter value : {{ counter() }}</span></li>
+    <li><span>Computed value (^2): {{ pow() }}</span></li>
+    <li><button class="btn btn-plain" (click)="increment()">Increment signal counter</button></li>
+  </ul>
   `,
-  styles: []
+  styles: [
+    `
+    li { 
+      list-style: none; 
+      height: 30px;
+    }
+    `
+  ]
 })
 export default class WelcomeComponent {
 
   @Input() routeInfo?: string;
+  counter = signal<number>(1);
+  pow = computed(() => Math.pow(this.counter(), 2));
 
   constructor() {
     inject(DestroyRef).onDestroy(() => {
       // observable unsubscriptions etc...
     })
+  }
+
+  increment() {
+    this.counter.update((old) => old + 1);
   }
 }
